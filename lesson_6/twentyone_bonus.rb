c583c395816cedac6fae127ef49a832c4f153ab7
Richard LoRicco <rtlo9962@gmail.com>
2022-07-27 13:09:02 -0400

    updating twentyone_bonus

--- lesson_6/twentyone_bonus.rb
+++ lesson_6/twentyone_bonus.rb
@@ -1,16 +1,3 @@
-=begin
-FLOW OF GAME
-1. Initialize deck
-2. Deal cards to player and dealer
-3. Player turn: hit or stay
-  - repeat until bust or "stay"
-4. If player bust, dealer wins.
-5. Dealer turn: hit or stay
-  - repeat until total >= 17
-6. If dealer bust, player wins.
-7. Compare cards and declare winner.
-=end
-
 SUITS = ['H', 'C', 'S', 'D']
 CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 POINT_LIMIT = 21
